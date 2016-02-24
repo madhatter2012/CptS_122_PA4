cde1a7484e2063e81cfad34fe8854b4746998856
Michael Smith <madhatter2012@gmail.com>
2016-02-23 17:44:40 -0800

    Start: Created class files, includes, using

---
+++ PA4/DietPlan.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::ostream;
+using std::istream;
+using std::fstream;
+using std::ifstream;
+using std::ofstream;
---
+++ PA4/FitnessAppWrapper.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::ostream;
+using std::istream;
+using std::fstream;
+using std::ifstream;
+using std::ofstream;
+
